5a36c567ca8cfeba4b0ee5d2ce1e9a2139b4cec7
MatthewKoch76 <matthewkoch76@gmail.com>
2018-07-16 03:04:59 -0500

    cocktail table

--- src/main/java/com/drinkthis/drinkthis/controllers/CocktailController.java
+++ src/main/java/com/drinkthis/drinkthis/controllers/CocktailController.java
@@ -6,6 +6,7 @@ import com.drinkthis.drinkthis.models.data.IngredientDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
